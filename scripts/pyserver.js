4f409d47015b60850276236d5fd9c53a0bfe6925
MaximLutay <maksym.lutai@nure.ua>
2017-06-04 19:25:41 +0300

    some docs on pyserver microscript

--- scripts/pyserver.js
+++ scripts/pyserver.js
@@ -3,12 +3,25 @@ const process = require("process");
 const spawn = require("child_process").spawn;
 
 
+//node ./scripts/pyserver.js 5000 childfoldername
+//node ./scripts/pyserver.js 5000
+//node ./scripts/pyserver.js childfoldername
+//node ./scripts/pyserver.js
+//are valid calls
+
+// where '5000'(8000 is default) is a port
+// 'childfoldername'("" is default) is child folder to serve from
 
 util.log(">>>",process.argv,"<<<<<<<<");
+
+let secondisport = process.argv[2].search(/^\d{4}$/g) ? true : false;
+
 let pyserver = spawn( "python"
-    ,["-m","http.server", +process.argv[2] || 8000 ]
+    ,["-m","http.server", +
+    secondisport ? process.argv[2] : false
+    || 8000 ]
     ,{
-      cwd: process.argv[3] || "",
+      cwd: secondisport ? process.argv[3] : process.argv[2] || "",
       env: process.env,
       stdio: ["ignore",process.stdout, process.stdout]
     }
